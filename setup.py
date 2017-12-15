171f9717c73419d31aea421b630f33a5ee8083ed
Graham Krizek <gkrizek@nodesource.com>
2017-12-15 08:01:36 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name="antmonitor",
+    version="0.1.0",
+    description="Antminer Monitoring Service",
+    long_description=readme(),
+    author="Graham Krizek",
+    author_email="graham@krizek.io",
+    url="https://github.com/gkrizek/antmonitor",
+    license="MIT",
+    packages=["antmonitor"],
+    install_requires=[
+        "boto3",
+        "requests"
+    ],
+    keywords="antminer bitcoin bitmain monitor btc bch cash"
+)
