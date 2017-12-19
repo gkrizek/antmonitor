0b8482796601f1d97c94fa41610f60a4bb12059f
Graham Krizek <gkrizek@nodesource.com>
2017-12-19 06:29:01 -0700

    Finish config file validation

--- antmonitor/utils.py
+++ antmonitor/utils.py
@@ -24,12 +24,12 @@ def Validate():
     # Miners
     try:
         config['miners']
-    except KeyError as e:
+    except KeyError:
         raise click.BadParameter('miners\n\nYou haven\'t specified any miners\nPlease add this section to your \'~/.antmonitor.cfg\'')
 
     try:
         config['miners']['antminers']
-    except KeyError as e:
+    except KeyError:
         raise click.BadParameter('antminers\n\nYou haven\'t specified any antminers\nPlease add this section to your \'~/.antmonitor.cfg\'')
 
     if len(config['miners']['antminers']) is 0:
@@ -38,15 +38,79 @@ def Validate():
     #Creds
     try:
         config['credentials']
-    except KeyError as e:
+    except KeyError:
         raise click.BadParameter('credentials\n\nYou haven\'t specified any credentials\nPlease add this section to your \'~/.antmonitor.cfg\'')
 
     try:
         config['credentials']['aws']
-    except KeyError as e:
+    except KeyError:
         raise click.BadParameter('aws\n\nYou haven\'t specified any aws credentials\nPlease add this section to your \'~/.antmonitor.cfg\'')
 
     try:
         config['credentials']['antminer']
-    except KeyError as e:
+    except KeyError:
         raise click.BadParameter('antminer\n\nYou haven\'t specified any antminer credentials\nPlease add this section to your \'~/.antmonitor.cfg\'')
+
+    try:
+        config['credentials']['antminer']['username']
+    except KeyError:
+        raise click.BadParameter('username\n\nYou haven\'t specified a username for the antminer ui\nPlease add this section to your \'~/.antmonitor.cfg\'')
+
+    try:
+        config['credentials']['antminer']['password']
+    except KeyError:
+        raise click.BadParameter('password\n\nYou haven\'t specified a password for the antminer ui\nPlease add this section to your \'~/.antmonitor.cfg\'')
+
+    #Alert
+    try:
+        config['alert']
+    except KeyError:
+        raise click.BadParameter('alert\n\nYou haven\'t specified any alert settings\nPlease add this section to your \'~/.antmonitor.cfg\'')
+
+    try:
+        config['alert']['notify']
+        if config['alert']['notify'] in [ True, 'true' ]:
+            try:
+                config['alert']['snstopic']
+            except KeyError:
+                raise click.BadParameter('snstopic\n\nYou haven\'t specified a sns topic\nPlease add this section to your \'~/.antmonitor.cfg\'')
+    except KeyError:
+        raise click.BadParameter('notify\n\nYou haven\'t specified a notification setting\nPlease add this section to your \'~/.antmonitor.cfg\'')
+
+    #Threshold
+    try:
+        config['threshold']
+    except KeyError:
+        raise click.BadParameter('threshold\n\nYou haven\'t specified any threshold settings\nPlease add this section to your \'~/.antmonitor.cfg\'')
+
+    try:
+        config['threshold']['temp']
+        try:
+            int(config['threshold']['temp'])
+        except ValueError:
+            raise click.BadParameter('temp\n\nThreshold of \'temp\' must be an integer\nPlease add this section to your \'~/.antmonitor.cfg\'')
+    except KeyError:
+        raise click.BadParameter('temp\n\nYou haven\'t specified any \'temp\' threshold settings\nPlease add this section to your \'~/.antmonitor.cfg\'')
+
+    try:
+        config['threshold']['memory']
+        try:
+            int(config['threshold']['memory'])
+        except ValueError:
+            raise click.BadParameter('memory\n\nThreshold of \'memory\' must be an integer\nPlease add this section to your \'~/.antmonitor.cfg\'')
+    except KeyError:
+        raise click.BadParameter('memory\n\nYou haven\'t specified any \'memory\' threshold settings\nPlease add this section to your \'~/.antmonitor.cfg\'')
+
+    try:
+        config['threshold']['pool']
+    except KeyError:
+        raise click.BadParameter('pool\n\nYou haven\'t specified any \'pool\' threshold settings\nPlease add this section to your \'~/.antmonitor.cfg\'')
+
+    try:
+        config['threshold']['hashes']
+        try:
+            int(config['threshold']['hashes'])
+        except ValueError:
+            raise click.BadParameter('hashes\n\nThreshold of \'hashes\' must be an integer\nPlease add this section to your \'~/.antmonitor.cfg\'')
+    except KeyError:
+        raise click.BadParameter('hashes\n\nYou haven\'t specified any \'hashes\' threshold settings\nPlease add this section to your \'~/.antmonitor.cfg\'')
