f76625cdae7683c3e9a3d5b70fe9197ddd5ed4a1
Graham Krizek <gkrizek@nodesource.com>
2018-01-13 01:31:18 -0700

    Add Temperature Check

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# TODO List
+
+- All hostname/IP of miner to alerts
--- antmonitor/check.py
+++ antmonitor/check.py
@@ -1,7 +1,36 @@
-
+from utils import GetConfig
+from alert import SendAlert
+import sys
+import click
 
 def TempCheck():
-    return
+    """
+    Get Temp Value
+    """
+    temp = 90
+    try:
+        config_temp = GetConfig('threshold,temp')
+    except KeyError as e:
+        raise click.UsageError('Configuration Failure\nCould not find temperature threshold')
+
+    context = click.get_current_context()
+    cli_cron = context.meta['cron']
+    cli_quiet = context.meta['quiet']
+
+    if temp > config_temp:
+        if cli_quiet:
+            sys.exit(1)
+        else:
+            alert = "Temperate too high. Received " + str(temp) + "."
+            #SendAlert(alert)
+            print(alert)
+            sys.exit(1)
+    else:
+        if cli_cron or cli_quiet:
+            return
+        else:
+            return "Temperature OK. Received " + str(temp) + "."
+
 
 def MemoryCheck():
     return
