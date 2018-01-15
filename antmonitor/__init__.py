f1babc4de5cbcceff695ad5c96e8c7688b89757f
Graham Krizek <gkrizek@nodesource.com>
2018-01-15 13:59:19 -0700

    Add current directory for modules

--- antmonitor/__init__.py
+++ antmonitor/__init__.py
@@ -1,6 +1,6 @@
-from alert import SendAlert
-from check import TempCheck, MemoryCheck, PoolCheck, HashCheck, AsicCheck, AllCheck
-from request import GetContent
-from utils import CreateConfig, GetConfig, GetMiners, Validate
+from .alert import SendAlert
+from .check import TempCheck, MemoryCheck, PoolCheck, HashCheck, AsicCheck, AllCheck
+from .request import GetContent
+from .utils import CreateConfig, GetConfig, GetMiners, Validate
 
 __version__ = '0.1.0'
